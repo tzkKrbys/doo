678a9d38e872f5f94428c6d345aa1235cc84ff0a
tzkkrbys <tzaugment@gmail.com>
2016-01-12 01:57:19 +0900

    app/controllers/users/omniauth_callbacks_controller.rb修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,6 +4,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     callback_from :twitter
   end
 
+  def facebook
+    callback_from :facebook
+  end
+
   private
 
   def callback_from(provider)
