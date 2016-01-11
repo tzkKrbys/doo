df0d19a7ecc2bb278aceff334bc8caa745b99b09
tzkkrbys <tzaugment@gmail.com>
2016-01-12 02:36:43 +0900

    rake db:migrate実行

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111170252) do
+ActiveRecord::Schema.define(version: 20160111173015) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "interest_id"
+    t.text     "body"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "interests", force: :cascade do |t|
     t.text     "title"
